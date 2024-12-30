eabe0b543131de5838dc2714ae9036bdbd138beb
Creteper <creteper@qq.com>
2024-12-30 19:45:34 +0800

    create titlebar

--- src/components/local/titlebar.tsx
+++ src/components/local/titlebar.tsx
@@ -1,11 +1,30 @@
 import { Button } from "../ui/button";
-import { X } from "lucide-react";
+import { X, Maximize, Minus } from "lucide-react";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 export function Titlebar() {
+    const window = getCurrentWindow()
+    function minimize() {
+        window.minimize()
+    }
+    function maximize() {
+        window.toggleMaximize()
+    }
+    function close() {
+        window.close()
+    }
     return (
-        <div data-tauri-drag-region className="h-[30px] select-none flex justify-end fixed top-0 left-0 right-0">
-            <Button variant={"ghost"} className="h-[30px] rounded-l-none rounded-b-none">
+        <div data-tauri-drag-region className="h-[30px] items-center select-none flex justify-end fixed top-0 left-0 right-0">
+            <p className="text-sm mr-auto ml-4">Areo To Do</p>
+            <Button onClick={minimize} variant={"ghost"} className="h-[30px] rounded-none rounded-b-none">
+                <Minus />
+            </Button>
+            <Button onClick={maximize} variant={"ghost"} className="h-[30px] rounded-none rounded-b-none">
+                <Maximize />
+            </Button>
+            <Button onClick={close} variant={"ghost"} className="h-[30px] rounded-l-none rounded-b-none">
                 <X />
             </Button>
+
         </div>
     )
 }
